47f8269515f32f1c1675d1ed8d6b00831fe87363
Lewis McGrath <lewismcgrath@Lewiss-MBP.qeopark.ford.com>
2019-07-31 18:15:46 +0100

    Add restart button to reset state

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -34,6 +34,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
     
+    @IBOutlet weak var restartButton: UIButton!
     // TODO Step 5: Initialise instance variables here
     
     var storyIndex: Int = 1
@@ -46,10 +47,8 @@ class ViewController: UIViewController {
         
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
-        storyTextView.text = story1
-        topButton.setTitle(answer1a, for: .normal)
-        bottomButton.setTitle(answer1b, for: .normal)
-        
+        restart()
+    
     }
 
     
@@ -93,9 +92,27 @@ class ViewController: UIViewController {
             storyIndex = 5
             
         }
-                
+        
+        if storyIndex == 4 || storyIndex == 5 || storyIndex == 6 {
+            restartButton.isHidden = false
+        }
+
         // TODO Step 6: Modify the IF-Statement to complete the story
+
+    }
+    @IBAction func restartButtonPressed(_ sender: Any) {
+       restart()
+        
+    }
     
+    func restart() {
+        storyTextView.text = story1
+        storyIndex = 1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+        topButton.isHidden = false
+        bottomButton.isHidden = false
+        restartButton.isHidden = true
     }
 }
 
